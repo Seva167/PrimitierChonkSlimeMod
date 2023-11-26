02469d4377e017934765300fbbb4b139b4cb7e4a
Seva167 <sewa.mikhailoff@yandex.ru>
2023-11-26 18:59:46 +0700

    Code commit

---
+++ PrimitierChonkSlimeMod/Mod.cs
@@ -0,0 +1,20 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2Cpp;
+
+namespace PrimitierChonkSlimeMod
+{
+    public class Mod : MelonMod
+    {
+
+    }
+
+    [HarmonyPatch(typeof(SlimeBase), nameof(SlimeBase.EatFood))]
+    public static class SlimeBaseEatFoodPatch
+    {
+        private static void Prefix(ref float efficiency)
+        {
+            efficiency = 100f;
+        }
+    }
+}
---
+++ PrimitierChonkSlimeMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using MelonLoader;
+using PrimitierChonkSlimeMod;
+
+[assembly: MelonInfo(typeof(Mod), "CHONK Slime mod", "1.0.0", "Seva167")]
+[assembly: MelonGame("PrimitierDev", "Primitier")]
